067d2e6ef8bd3ff543b7eb636ad1b493ca4873fa
Riddhi Sibal <sibal.riddhi@gmail.com>
2026-03-26 23:41:03 +0530

    Update AuditLogger.java

--- database/AuditLogger.java
+++ database/AuditLogger.java
@@ -15,7 +15,7 @@ public class AuditLogger {
     
         String sql = "INSERT INTO audit_log (action, target, details) VALUES (?, ?, ?)";
         try (Connection c = DBConnection.get();
-             PreparedStatement ps = c.prepareStatement(sql)) {
+             PreparedStatement ps = c.prepareStatement(sql1)) {
             ps.setString(1, action);
             ps.setString(2, target);
             ps.setString(3, details);
